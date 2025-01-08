5f3e71f47df388428d280a23096af643ce5413ef
JulienPeloton <peloton@lal.in2p3.fr>
2025-01-08 12:21:32 +0100

    Fix number

--- lib/display.py
+++ lib/display.py
@@ -77,7 +77,7 @@ def main(width=240, height=240, progression=120000):
 
     size = 35
     font = ImageFont.truetype("fonts/DS-DIGIB.TTF", size)
-    draw.text((width/2, width/2 - size / 3), "120k", anchor="mt", font=font)
+    draw.text((width/2, width/2 - size / 3), "{}k".format(int(progression/1000)), anchor="mt", font=font)
 
     # Draw polygone de la meme maniere que les traits (x0, y0, ...)
     # Angles are measured from 3 o’clock, increasing clockwise.
